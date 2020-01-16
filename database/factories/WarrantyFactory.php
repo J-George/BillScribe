2c623fe8e0b2ead6105937f3f1fc91cb686db27a
Justin George <george.justin16@me.com>
2020-01-15 20:15:22 -0400

    Warranty Factory

--- database/factories/WarrantyFactory.php
+++ database/factories/WarrantyFactory.php
@@ -4,6 +4,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Warranty::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->company,
+        'category' => $faker->lastName,
+        'brand' => 'Apple',
+        'cost' => $faker->randomNumber($nbDigits = 4, $strict = false),
+        'dateOfPurchase' => now(),
+        'placeOfPurchase' => $faker->streetName,
+        'serialNumber' => str_random(10),
     ];
 });
